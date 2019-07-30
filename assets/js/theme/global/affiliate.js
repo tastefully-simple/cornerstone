892991f3e0f8f2315ad0a109056341176467acad
Denis Priebe <denis.priebe@irishtitan.com>
2019-07-30 18:36:08 -0500

    Add "wait" function for Social Bug.

--- assets/js/theme/global/affiliate.js
+++ assets/js/theme/global/affiliate.js
@@ -1,10 +1,27 @@
 /**
- * This function grabs the affiliate id from the Social Bug <div> tag and
- * appends it to the "Shop more" button.
+ * The Social Bug plugin does not load right away with the rest of the assets
+ * on the page. We need to wait/check for the plugin until it is available.
+ *
+ * Ideally, Social Bug should fire off an event when it is fully loaded or add
+ * the affiliate id to BigCommerce's global configuration object.
+ */
+function waitForSocialBug(callback) {
+    if ($('#affiliatediv').length) {
+        callback();
+    } else {
+        setTimeout(() => waitForSocialBug(callback), 500);
+    }
+}
+
+/**
+ * This function grabs the affiliate id from the Social Bug <div> tag once it's
+ * loaded and appends it to the "Shop more" button.
  */
 export default function () {
-    const affiliateId = $('#affiliatediv').data('affiliateid');
-    const affiliateParam = affiliateId !== undefined ? `&afid=${affiliateId}` : '';
+    waitForSocialBug(() => {
+        const affiliateId = $('#affiliatediv').data('affiliateid');
+        const affiliateParam = affiliateId !== undefined ? `&afid=${affiliateId}` : '';
 
-    $('#shop-more-btn').attr('href', (i, href) => href + affiliateParam);
+        $('#shop-more-btn').attr('href', (i, href) => href + affiliateParam);
+    });
 }

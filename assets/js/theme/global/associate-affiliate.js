a04f5cc3f7c42afaca3b81bea2eb7233283862aa
Denis Priebe <denis.priebe@irishtitan.com>
2019-10-18 10:33:16 -0500

    Add feature documentation and clean up code.

--- assets/js/theme/global/associate-affiliate.js
+++ assets/js/theme/global/associate-affiliate.js
@@ -1,20 +1,31 @@
-import axios from 'axios';
-
 /**
  * This value represents the actual query string we are checking for in the URL
  * on page load. It should have the affiliate ID as a value.
  */
-const AFID = 'AFID';
+const afid = 'AFID';
 
 /**
- * This function grabs the affiliate id from the Social Bug <div> tag once it's
- * loaded and appends it to the "Shop more" button.
+ * This value represents the base "short url" which transitions over to the
+ * longer URL. We will append the AFID value from above to this URL and set
+ * it as the source of the iframe.
+ */
+const src = 'https://tastefullysimpl.sb-affiliate.com/r66/';
+
+/**
+ * This function is responsible for creating an iframe which in turn should
+ * set the appropriate Social Bug affiliate cookie for the current user.
  */
 export default function () {
     const params = new URLSearchParams(window.location.search);
 
-    if (params.has(AFID)) {
-        
-        console.log(params.get(AFID));
+    if (params.has(afid)) {
+        const affiliate = params.get(afid);
+        const frame = document.createElement('iframe');
+
+        frame.style.display = 'none';
+        frame.src = `${src}${affiliate}`;
+
+        document.body.appendChild(frame);
     }
 }
+

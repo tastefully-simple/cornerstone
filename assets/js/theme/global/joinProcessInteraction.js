db59d2331ab6c9937a615bb1ce7dd92938a05ccd
Katie Mangan <katie.mangan@irishtitan.com>
2020-01-23 13:46:34 -0600

    esLint fixes

--- assets/js/theme/global/joinProcessInteraction.js
+++ assets/js/theme/global/joinProcessInteraction.js
@@ -1,5 +1,5 @@
-const loginPage         = document.getElementById('join-login');
-const personalInfoPage = document.getElementById('personal-info')
+const loginPage = document.getElementById('join-login');
+const personalInfoPage = document.getElementById('personal-info');
 
 /**
  * This function will remove the container class on the page wrapper for join pages. This is to allow full width banner.
@@ -12,11 +12,11 @@ function removeContainer() {
  * This function will show or hide form sign up fields when toggle is checked.
  */
 function toggleStyles() {
-    const checkbox  = loginPage.querySelector('#form-toggle');
-    const signUp    = loginPage.querySelector('#sign-up');
-    const login     = loginPage.querySelector('#login');
+    const checkbox = loginPage.querySelector('#form-toggle');
+    const signUp = loginPage.querySelector('#sign-up');
+    const login = loginPage.querySelector('#login');
     const firstName = loginPage.querySelector('#firstNameField');
-    const lastName  = loginPage.querySelector('#lastNameField');
+    const lastName = loginPage.querySelector('#lastNameField');
     const password2 = loginPage.querySelector('#password2Field');
 
     checkbox.addEventListener('change', (event) => {
@@ -41,16 +41,14 @@ function toggleStyles() {
  * This function will change value styling once a value is selected.
  */
 function styleSelectValue() {
-    const dates     = personalInfoPage.querySelectorAll('[type="date"]');
+    const dates = personalInfoPage.querySelectorAll('[type="date"]');
     const selectors = personalInfoPage.querySelectorAll('[type="select"]');
 
-    function addChangeHandler(elementArray){
+    function addChangeHandler(elementArray) {
         elementArray.forEach((element) => {
-
-            element.addEventListener('change', (event) => {
+            element.addEventListener('change', () => {
                 element.classList.remove('empty');
             });
-
         });
     }
     if (dates) { addChangeHandler(dates); }
@@ -61,8 +59,8 @@ function styleSelectValue() {
  * This function will hide primary phone field when 'cell is primary' is checked.
  */
 function togglePhoneConditionalField() {
-    const phoneCheckbox   = personalInfoPage.querySelector('#MobilePhoneCheckbox');
-    const primaryPhone    = personalInfoPage.querySelector('#PrimaryPhone');
+    const phoneCheckbox = personalInfoPage.querySelector('#MobilePhoneCheckbox');
+    const primaryPhone = personalInfoPage.querySelector('#PrimaryPhone');
     const primaryPhoneDiv = personalInfoPage.querySelector('#primaryPhoneField');
 
     phoneCheckbox.addEventListener('change', (event) => {
@@ -80,7 +78,7 @@ function togglePhoneConditionalField() {
  * This function will hide billing address fields when 'shipping same as billing' is checked.
  */
 function toggleAddressConditionalFields() {
-    const addressCheckbox  = personalInfoPage.querySelector('#AddressCheckbox');
+    const addressCheckbox = personalInfoPage.querySelector('#AddressCheckbox');
     const shippingFieldset = personalInfoPage.querySelector('#shipping-address');
 
     addressCheckbox.addEventListener('change', (event) => {
@@ -99,7 +97,7 @@ function toggleTsCashConditionalField() {
     const cashOption = personalInfoPage.querySelector('#TsCashOption');
     const otherField = personalInfoPage.querySelector('#tsCashOptionTextField');
 
-    cashOption.addEventListener('change', (event) => {
+    cashOption.addEventListener('change', () => {
         const strCashOption = cashOption.options[cashOption.selectedIndex].text;
         if (strCashOption === 'Other:') {
             otherField.classList.remove('hidden');
@@ -107,31 +105,27 @@ function toggleTsCashConditionalField() {
             otherField.classList.add('hidden');
         }
     });
-
 }
-
 /**
  * This function will show modal on link click.
  */
 function openTermsModal() {
     const termsModal = personalInfoPage.querySelector('#terms-modal');
-    const modalLink  = personalInfoPage.querySelector('#openTermsModal');
+    const modalLink = personalInfoPage.querySelector('#openTermsModal');
 
-    modalLink.addEventListener('click', (event) => {
+    modalLink.addEventListener('click', () => {
         termsModal.classList.add('join__modal-overlay--active');
     });
-
 }
-
 /**
  * This function will close the terms modal on icon click.
  */
 function closeTermsModal() {
-    const termsModal  = personalInfoPage.querySelector('#terms-modal');
-    const closeIcons  = personalInfoPage.querySelectorAll('.terms-close');
+    const termsModal = personalInfoPage.querySelector('#terms-modal');
+    const closeIcons = personalInfoPage.querySelectorAll('.terms-close');
 
     closeIcons.forEach((closeIcon) => {
-        closeIcon.addEventListener('click', (event) => {
+        closeIcon.addEventListener('click', () => {
             termsModal.classList.remove('join__modal-overlay--active');
         });
     });
@@ -141,10 +135,10 @@ function closeTermsModal() {
  * Trigger submit on checkout button click.
  */
 function triggerSubmit() {
-    const infoForm       = personalInfoPage.querySelector('#frmJoinPersonalInfoTest');
+    const infoForm = personalInfoPage.querySelector('#frmJoinPersonalInfoTest');
     const checkoutButton = personalInfoPage.querySelector('#checkout');
 
-    checkoutButton.addEventListener('click', (event) => {
+    checkoutButton.addEventListener('click', () => {
         infoForm.submit();
     });
 }
@@ -153,10 +147,10 @@ function triggerSubmit() {
  * Trigger terms checkbox on form when external checkbox is clicked.
  */
 function triggerTermsApprove() {
-    const visibleCheckbox   = personalInfoPage.querySelector('#TermsCheckboxVisible');
+    const visibleCheckbox = personalInfoPage.querySelector('#TermsCheckboxVisible');
     const invisibleCheckbox = personalInfoPage.querySelector('#TermsCheckbox');
 
-    visibleCheckbox.addEventListener('change', (event) => {
+    visibleCheckbox.addEventListener('change', () => {
         if (visibleCheckbox.checked === true) {
             invisibleCheckbox.checked = true;
         } else {
@@ -170,13 +164,12 @@ function triggerTermsApprove() {
  * Export join process front end functions.
  */
 export default function joinProcessInteraction() {
-
-    //call functions on join page
+    // call functions on join page
     if (loginPage) {
         removeContainer();
         toggleStyles();
     }
-    //call functions on tell us about yourself page
+    // call functions on tell us about yourself page
     if (personalInfoPage) {
         removeContainer();
         styleSelectValue();

0916012713b7258de52ca6a74aaa74672be9151c
Madeline Graham <mgraham@irishtitan-mgraham-pro.local>
2020-01-23 09:46:49 -0600

    Change function names to begin with verbs, move consts to top of file

--- assets/js/theme/global/joinProcessInteraction.js
+++ assets/js/theme/global/joinProcessInteraction.js
@@ -1,3 +1,26 @@
+const checkbox          = document.getElementById('form-toggle');
+const signUp            = document.getElementById('sign-up');
+const login             = document.getElementById('login');
+const firstName         = document.getElementById('firstNameField');
+const lastName          = document.getElementById('lastNameField');
+const password2         = document.getElementById('password2Field');
+const dates             = document.querySelectorAll('[type="date"]');
+const selectors         = document.querySelectorAll('[type="select"]');
+const phoneCheckbox     = document.getElementById('MobilePhoneCheckbox');
+const primaryPhone      = document.getElementById('PrimaryPhone');
+const primaryPhoneDiv   = document.getElementById('primaryPhoneField');
+const addressCheckbox   = document.getElementById('AddressCheckbox');
+const shippingFieldset  = document.getElementById('shipping-address');
+const cashOption        = document.getElementById('TsCashOption');
+const otherField        = document.getElementById('tsCashOptionTextField');
+const termsModal        = document.getElementById('terms-modal');
+const modalLink         = document.getElementById('openTermsModal');
+const closeIcons        = document.getElementsByClassName('terms-close');
+const infoForm          = document.getElementById('frmJoinPersonalInfoTest');
+const checkoutButton    = document.getElementById('checkout');
+const visibleCheckbox   = document.getElementById('TermsCheckboxVisible');
+const invisibleCheckbox = document.getElementById('TermsCheckbox');
+
 /**
  * This function will remove the container class on the page wrapper for join pages. This is to allow full width banner.
  */
@@ -11,12 +34,6 @@ function removeContainer() {
  * This function will show or hide form sign up fields when toggle is checked.
  */
 function toggleStyles() {
-    const checkbox  = document.getElementById('form-toggle');
-    const signUp    = document.getElementById('sign-up');
-    const login     = document.getElementById('login');
-    const firstName = document.getElementById('firstNameField');
-    const lastName  = document.getElementById('lastNameField');
-    const password2 = document.getElementById('password2Field');
     if (checkbox) {
         checkbox.addEventListener('change', (event) => {
             if (event.target.checked) {
@@ -39,14 +56,14 @@ function toggleStyles() {
 /**
  * This function will change value styling once a value is selected.
  */
-function selectStyling() {
-    const dates = document.querySelectorAll('[type="date"]');
-    const selectors = document.querySelectorAll('[type="select"]');
+function styleSelectValue() {
     function addChangeHandler(elementArray){
         elementArray.forEach((element) => {
+
             element.addEventListener('change', (event) => {
                 element.classList.remove('empty');
             });
+
         });
     }
     if (dates) { addChangeHandler(dates); }
@@ -56,69 +73,66 @@ function selectStyling() {
 /**
  * This function will hide primary phone field when 'cell is primary' is checked.
  */
-function phoneConditionalFields() {
-    const phoneCheckbox = document.getElementById('MobilePhoneCheckbox');
-    phoneCheckbox.addEventListener('change', (event) => {
-        const primaryPhone = document.getElementById('PrimaryPhone');
-        const primaryPhoneDiv = document.getElementById('primaryPhoneField');
-        if (!event.target.checked) {
-            primaryPhoneDiv.classList.remove('disabled');
-            primaryPhone.removeAttribute('disabled');
-        } else {
-            primaryPhoneDiv.classList.add('disabled');
-            primaryPhone.setAttribute('disabled', 'disabled');
-        }
-    });
+function togglePhoneConditionalField() {
+    if (phoneCheckbox) {
+        phoneCheckbox.addEventListener('change', (event) => {
+            if (!event.target.checked) {
+                primaryPhoneDiv.classList.remove('disabled');
+                primaryPhone.removeAttribute('disabled');
+            } else {
+                primaryPhoneDiv.classList.add('disabled');
+                primaryPhone.setAttribute('disabled', 'disabled');
+            }
+        });
+    }
 }
 
 /**
  * This function will hide billing address fields when 'shipping same as billing' is checked.
  */
-function addressConditionalFields() {
-    const addressCheckbox = document.getElementById('AddressCheckbox');
-    addressCheckbox.addEventListener('change', (event) => {
-        const shippingFieldset = document.getElementById('shipping-address');
-        if (!event.target.checked) {
-            shippingFieldset.classList.remove('hidden');
-        } else {
-            shippingFieldset.classList.add('hidden');
-        }
-    });
+function toggleAddressConditionalFields() {
+    if (addressCheckbox) {
+        addressCheckbox.addEventListener('change', (event) => {
+            if (!event.target.checked) {
+                shippingFieldset.classList.remove('hidden');
+            } else {
+                shippingFieldset.classList.add('hidden');
+            }
+        });
+    }
 }
 
 /**
  * This function will show the "other" text field when Other is the option selected in TS Cash dropdown.
  */
-function tsCashConditionalOtherField() {
-    const cashOption = document.getElementById('TsCashOption');
-    const otherField = document.getElementById('tsCashOptionTextField')
-    cashOption.addEventListener('change', (event) => {
-        const strCashOption = cashOption.options[cashOption.selectedIndex].text;
-        if (strCashOption === 'Other:') {
-            otherField.classList.remove('hidden');
-        } else {
-            otherField.classList.add('hidden');
-        }
-    });
+function toggleTsCashConditionalField() {
+    if (cashOption) {
+        cashOption.addEventListener('change', (event) => {
+            const strCashOption = cashOption.options[cashOption.selectedIndex].text;
+            if (strCashOption === 'Other:') {
+                otherField.classList.remove('hidden');
+            } else {
+                otherField.classList.add('hidden');
+            }
+        });
+    }
 }
 
 /**
  * This function will show modal on link click.
  */
 function openTermsModal() {
-    const termsModal = document.getElementById('terms-modal');
-    const modalLink = document.getElementById('openTermsModal');
-    modalLink.addEventListener('click', (event) => {
-        termsModal.classList.add('join__modal-overlay--active');
-    });
+    if (modalLink) {
+        modalLink.addEventListener('click', (event) => {
+            termsModal.classList.add('join__modal-overlay--active');
+        });
+    }
 }
 
 /**
  * This function will close the terms modal on icon click.
  */
 function closeTermsModal() {
-    const termsModal = document.getElementById('terms-modal');
-    const closeIcons = document.getElementsByClassName('terms-close');
     closeIcons.forEach((closeIcon) => {
         closeIcon.addEventListener('click', (event) => {
             termsModal.classList.remove('join__modal-overlay--active');
@@ -130,26 +144,26 @@ function closeTermsModal() {
  * Trigger submit on checkout button click.
  */
 function triggerSubmit() {
-    const infoForm = document.getElementById('frmJoinPersonalInfoTest');
-    const checkoutButton = document.getElementById('checkout');
-    checkoutButton.addEventListener('click', (event) => {
-        infoForm.submit();
-    });
+    if (checkoutButton) {
+        checkoutButton.addEventListener('click', (event) => {
+            infoForm.submit();
+        });
+    }
 }
 
 /**
  * Trigger terms checkbox on form when external checkbox is clicked.
  */
 function triggerTermsApprove() {
-    const visibleCheckbox = document.getElementById('TermsCheckboxVisible');
-    const invisibleCheckbox = document.getElementById('TermsCheckbox');
-    visibleCheckbox.addEventListener('change', (event) => {
-        if (visibleCheckbox.checked === true) {
-            invisibleCheckbox.checked = true;
-        } else {
-            invisibleCheckbox.checked = false;
-        }
-    });
+    if (visibleCheckbox) {
+        visibleCheckbox.addEventListener('change', (event) => {
+            if (visibleCheckbox.checked === true) {
+                invisibleCheckbox.checked = true;
+            } else {
+                invisibleCheckbox.checked = false;
+            }
+        });
+    }
 }
 
 
@@ -159,10 +173,10 @@ function triggerTermsApprove() {
 export default function joinProcessInteraction() {
     removeContainer();
     toggleStyles();
-    selectStyling();
-    phoneConditionalFields();
-    addressConditionalFields();
-    tsCashConditionalOtherField();
+    styleSelectValue();
+    togglePhoneConditionalField();
+    toggleAddressConditionalFields();
+    toggleTsCashConditionalField();
     openTermsModal();
     closeTermsModal();
     triggerSubmit();

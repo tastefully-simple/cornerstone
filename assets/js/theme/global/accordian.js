956298205c3302a231cfb48a671b71d3df4609e4
William Byrne <wbyrne@razoyo.com>
2020-05-19 14:14:25 -0500

    Fix accordion grouping

--- assets/js/theme/global/accordian.js
+++ assets/js/theme/global/accordian.js
@@ -1,14 +1,37 @@
 class Accordian {
     constructor($accordian) {
+        this.openItem = null;
+
         this.$accordian = $accordian;
-        this.$title = $accordian.querySelector('.tst-accordian-title');
-        this.$body = $accordian.querySelector('.tst-accordian-body');
+        let itemsArr = Array.from($accordian.querySelectorAll('.tst-accordian-item'));
+        this.items = itemsArr.map((el) => {
+          return new AccordianItem(el, this);
+        });
+    }
+
+    itemOpened(item) {
+        if (this.openItem) {
+            this.openItem.close();
+        }
+        this.openItem = item;
+    }
+}
+
+class AccordianItem {
+    constructor($item, accordian) {
+        this.isOpen = false;
+
+        // DOM
+        this.$item = $item;
+        this.$title = $item.querySelector('.tst-accordian-title');
+        this.$body = $item.querySelector('.tst-accordian-body');
+        this.accordian = accordian
 
         this.initListeners();
     }
 
     initListeners() {
-        this.$accordian.addEventListener(
+        this.$item.addEventListener(
             'click',
             () => this.onClick(),
             false
@@ -16,13 +39,31 @@ class Accordian {
     }
 
     onClick() {
-        this.$accordian.classList.toggle('open');
+        console.log(this);
+        if (this.isOpen) {
+            this.close();
+        } else {
+            this.open();
+        }
+    }
+
+    open() {
+        console.log('opening...');
+        this.$item.classList.add('open');
+        this.isOpen = true;
+        this.accordian.itemOpened(this);
+    }
+
+    close() {
+        console.log('closing...');
+        this.$item.classList.remove('open');
+        this.isOpen = false;
     }
 }
 
 export default function () {
-    let accordians = $('.tst-accordian');
-    accordians.each((i, elem) => {
+    let accordian = $('.tst-accordian');
+    accordian.each((i, elem) => {
         new Accordian(elem);
     });
 }

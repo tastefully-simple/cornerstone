2c80c3fb9f427ce7dadb2ec2cc8a7aceeae3a49f
Jasmine Galang <jgalang@razoyo.com>
2020-05-19 17:46:09 +0000

    TST-8 Applied the stylings following the mockups and styleguide
    - Added an event listener when closing the alertbox

--- assets/js/theme/razoyo/newsletter-alert.js
+++ assets/js/theme/razoyo/newsletter-alert.js
@@ -1,5 +1,3 @@
-import $ from 'jquery';
-
 function checkEmail(email) {
     const url = 'https://qa1-tsapi.tastefullysimple.com/users/welcome/check';
 
@@ -14,16 +12,44 @@ function checkEmail(email) {
     .catch(err => console.log('checkEmail Error', err));
 }
 
+function addAlertBox(status, title, message) {
+    const $alertBox = document.createElement('div');
+    $alertBox.className = `alertbox-${status}`;
+
+    const $alertTitle = document.createElement('h2');
+    $alertTitle.classList.add('alert-title');
+    $alertTitle.innerHTML = title;
+
+    const $alertMessage = document.createElement('p');
+    const $alertMessageSpan = document.createElement('span');
+    $alertMessage.classList.add('alert-message');
+    $alertMessageSpan.innerHTML = message;
+    $alertMessage.appendChild($alertMessageSpan);
+
+    $alertBox.appendChild($alertTitle);
+    $alertBox.appendChild($alertMessage);
+
+    if (status === 'error') {
+        const $retry = document.createElement('a');
+        $retry.classList.add('retry-btn');
+        $retry.classList.add('framelink-md');
+        $retry.innerHTML = 'retry';
+        $alertBox.appendChild($retry);
+    }
+
+    return $alertBox;
+}
+
 export default function (e) {
-    const $form = document.querySelector('footer article[data-section-type="newsletterSubscription"] form');
-    const $message = document.createElement('p');
-    $message.style.color = '#FFFFFF';
-    $form.prepend($message);
+    const $form = document.querySelector('footer div[data-section-type="newsletterSubscription"] form');
+    const $alertBoxContainer = document.createElement('div');
+    $alertBoxContainer.classList.add('alertbox-container');
 
     $form.addEventListener('submit', function(e) {
         e.preventDefault();
 
-        const formData = new FormData(this); // this = form element
+        const self = this; // this = form element
+        const formData = new FormData(self);
         const $inputEmail = document.querySelector('footer #nl_email');
 
         fetch('/subscribe.php?action=subscribe', {
@@ -36,25 +62,56 @@ export default function (e) {
 
             checkEmail($inputEmail.value)
             .then(res => {
+                let $alertBox;
                 if (resUrl === `${url}success` && res === '"OK"') {
-                    $message.innerHTML = '';
-                    $message.innerHTML = `Success! You successfully subscribed. 
-                      Use promo code: 000000 for 10% off on your $60 purchase.`;
-                } else if (resUrl === `${url}already_subscribed` || res === '"OK"') {
-                    $message.innerHTML = '';
-                    $message.innerHTML = 'You have already subscribed';
+                    const title = 'Good things are coming your way!';
+                    const message = 'Use promo code: 00000 for 10% off on your $60 purchase.';
+                    $alertBox = addAlertBox('success', title, message);
+                    $alertBoxContainer.innerHTML = '';
+                    $alertBoxContainer.appendChild($alertBox);
+                } else if (resUrl === `${url}already_subscribed`) {
+                    $alertBox = addAlertBox('error', 'Error', 'You have already been subscribed.');
+                    $alertBoxContainer.innerHTML = '';
+                    $alertBoxContainer.appendChild($alertBox);
                 } else if (res === '"Please send the email address to check"') {
-                    $message.innerHTML = '';
-                    $message.innerHTML = 'Error! Blank email';
+                    $alertBox = addAlertBox('error', 'Error', 'Please enter a valid email address.');
+                    $alertBoxContainer.innerHTML = '';
+                    $alertBoxContainer.appendChild($alertBox);
                 } else if (resUrl === `${url}success`) {
-                    $message.innerHTML = '';
-                    $message.innerHTML = 'Success! You have been subscribed';
+                    $alertBox = addAlertBox('success', 'Success', 'You have been subscribed.');
+                    $alertBoxContainer.innerHTML = '';
+                    $alertBoxContainer.appendChild($alertBox);
                 } else {
-                    $message.inner = '';
-                    $message.innerHTML = 'Error!';
+                    $alertBox = addAlertBox('error', 'Error', 'Something went wrong');
+                    $alertBoxContainer.innerHTML = '';
+                    $alertBoxContainer.appendChild($alertBox);
+                }
+
+                // Prevents the inner element ($alertBox) being clicked when $alertBoxContainer is clicked
+                $alertBox.addEventListener('click', (e) => e.stopPropagation());
+                self.appendChild($alertBoxContainer);
+
+                const $retry = document.querySelector('.alertbox-container a');
+                if ($retry) {
+                    const $input = document.querySelector('form .newsletter-inputs input');
+                    $retry.addEventListener('click', function() {
+                        $input.focus();
+                        self.removeChild($alertBoxContainer);
+                    });
                 }
             });
         });
     });
-}
 
+    // Closes the alertbox when the outside of alertbox is clicked
+    $alertBoxContainer.addEventListener('click', function(e) {
+        const $retry = document.querySelector('.alertbox-container a');
+
+        if ($retry) {
+            const $input = document.querySelector('form .newsletter-inputs input');
+            $input.focus();
+        }
+
+        $form.removeChild(this);
+    });
+}

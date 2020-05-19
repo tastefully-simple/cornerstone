b686ce28168b48d7fd8208f1772b62c2f943e04b
William Byrne <wbyrne@razoyo.com>
2020-05-19 07:46:33 -0500

    Remove un-needed comment from accordian.js

--- assets/js/theme/global/accordian.js
+++ assets/js/theme/global/accordian.js
@@ -26,20 +26,3 @@ export default function () {
         new Accordian(elem);
     });
 }
-
-/*
-    var accItem = document.getElementsByClassName('footer-info-col--small');
-    var accHD = document.getElementsByClassName('footer-info-heading acc-head');
-    for (i = 0; i < accHD.length; i++) {
-        accHD[i].addEventListener('click', toggleItem, false);
-    }
-    function toggleItem() {
-        var itemClass = this.parentNode.className;
-        for (i = 0; i < accItem.length; i++) {
-            accItem[i].className = 'footer-info-col footer-info-col--small close';
-        }
-        if (itemClass == 'footer-info-col footer-info-col--small close') {
-            this.parentNode.classList.add('open').classList.remove('close');
-        }
-    }
-*/

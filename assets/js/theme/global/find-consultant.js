6413f7009d856dc1179005dd384f3009601d419d
gbyrne <gbyrne@razoyo.com>
2020-06-09 16:25:57 -0500

    TST-62 - Initial work for 1 hardcoded consultant result finished

--- assets/js/theme/global/find-consultant.js
+++ assets/js/theme/global/find-consultant.js
@@ -1,8 +1,24 @@
 import utils from '@bigcommerce/stencil-utils';
 import { defaultModal } from '../global/modal';
-import $ from 'jquery';
 
-export default function(context) {
+/**
+ * Makes an API call to retrieve consultants (based off of search parameters)
+ * and returns the data in a json object
+ */
+function getConsultantSearchResults(searchParameters) {
+    var json = {
+      "item1": 1,
+      "item2": 2,
+    };
+    return json;
+}
+
+/**
+ * Creates a Cornerstone popup modal for Find A Consultant. 
+ * A second view is available once the user hits search. The modal is then 
+ * populated with data from the user's search parameters
+ */
+export default function() {
   $(document).ready(function() {
     const modal = defaultModal();
     $('.headertoplinks-consult').on('click', (e) => {
@@ -17,6 +33,11 @@ export default function(context) {
           modal.updateContent(res);
         }
       });
+    });
+
+    $('body').on('click', '.button-alternate', function() {
+        $("#consultant-search").hide();
+        $("#consultant-search-results").show();
     });
   });
 }

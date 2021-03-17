7596d3490d6b05076afbb7ca056cd11993a31776
Jasmine Galang <jgalang@razoyo.com>
2021-03-17 08:04:11 -0500

    TST-331 Task 547
    Retire old join process. Deleted join files.

--- assets/js/theme/common/ts-cart.js
+++ assets/js/theme/common/ts-cart.js
@@ -1,20 +1,13 @@
 import PageManager from '../page-manager';
 import TSAffiliationCheck from './ts-affiliation-check';
-import TSJoinAbandonment from './ts-join-abandonment';
 
 export default class TSCart extends PageManager {
     onReady() {
         this.initTSAffiliationCheck();
-        this.initTSJoinAbandonment();
     }
 
     initTSAffiliationCheck() {
         const tsAffiliationCheck = new TSAffiliationCheck();
         return tsAffiliationCheck;
-    }
-
-    initTSJoinAbandonment() {
-        const tsJoinAbandonment = new TSJoinAbandonment();
-        return tsJoinAbandonment;
     }
 }

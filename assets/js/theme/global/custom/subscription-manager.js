b08843ed1f89c57bfadec26cf0bc42215349491a
Tiago Rodrigues <tiagor@razoyo.com>
2023-07-21 17:07:44 -0300

    TST-993 - TASK 5142: Subscription Items list check - change to Bold storefront data

--- assets/js/theme/global/custom/subscription-manager.js
+++ assets/js/theme/global/custom/subscription-manager.js
@@ -295,17 +295,31 @@ function getAutoshipProducts(subscriptionManagement) {
     });
 }
 
-function toggleAutoshipButtons(subscriptionManagement) {
-    // Fetches a list of products
-    // [timeout: int, products: array[]]
-    const autoshipData = window.localStorage.getItem('subscription-products') ?
-        JSON.parse(window.localStorage.getItem('subscription-products')) : false;
+function isSubscriptionProduct(productId) {
+    let data = window.BOLD.subscriptions.data;
+    let foundGroup = null;
+    for (let i = 0; i < data.subscriptionGroups.length; i += 1) {
+        let subscriptionGroup = data.subscriptionGroups[i];
+        for (let j = 0; j < subscriptionGroup.selection_options.length; j += 1) {
+            let selectionOption = subscriptionGroup.selection_options[j];
+            if (selectionOption.platform_entity_id === productId) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
 
-    if (!autoshipData || autoshipData.timeout < new Date().getTime()) {
-        getAutoshipProducts(subscriptionManagement);
-    } else {
-        // Data is still valid. Display the current products on the local storage
-        displayAutoshipButtonForProducts(autoshipData.products);
+function toggleAutoshipButtons() {
+    const onPageProducts = document.getElementsByName('product_id');
+    const subscriptionProductIds = [];
+    for (let i = 0; i < onPageProducts.length; i++) {
+        if (isSubscriptionProduct(onPageProducts[i].defaultValue)) {
+            subscriptionProductIds.push(onPageProducts[i].defaultValue);
+        }
+    }
+    if (subscriptionProductIds.length > 0) {
+        displayAutoshipButtonForProducts(subscriptionProductIds);
     }
 }
 
@@ -323,7 +337,7 @@ export default function (customerId, productId, subscriptionManagement, customer
 
     // Toggle Autoship buttons after DOM is ready
     $(document).ready(() => {
-        toggleAutoshipButtons(subscriptionManagement);
+        toggleAutoshipButtons();
     });
 
     if (productId === undefined) {
